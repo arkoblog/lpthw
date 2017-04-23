d8f405abe09d0c79f66596e162c0e6cab947854c
akoira <akoira>
2017-04-23 11:47:54 +0545

    Completed till ex6.py

---
+++ ex3.py
@@ -0,0 +1,26 @@
+print "I will now count my chickens:"
+
+# Sums up hens
+print "Hens", 25 + 30 / 6
+# Count roosters
+print "Roosters", 100 - 25 * 3 % 4
+
+# 6-5+0-1/4+6
+# Do some random math
+print 3 + 2 + 1 - 5 + 4.0 % 2.0 - 1 / 4.0 + 6
+
+# Check a condition
+print "Is it true that 3 + 2 < 5 - 7?"
+print 3 + 2 < 5 - 7
+
+# Calculate and display results
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7? ", 5 - 7
+
+# Oh, that's why moment
+print "Oh, that's why it's False."
+
+# Other conditional checks
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
